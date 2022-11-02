b5d86983765917fb4951da8f1954ff2ce5ee9470
lzjlsy999 <112147835+lzjlsy999@users.noreply.github.com>
2022-11-02 21:45:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVBg6XMEoLcUMCrNVuYu6NPt_Zs',
+      id: 'oyVBg6aUUhYMa_e0BptR5614vby4',
     }
   ],
 
